09af5c3105a6bf7d821fe00e064d004628c1b982
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-30 10:35:43 +0300

    Updated .env file

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ import express from "express"
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.get("/api/upload",(req,res)=>{
+    res.send("It works!")
+})
 
-// app.listen(port,())
+
+app.listen(port,()=>{
+    console.log("Server running on 3000")
+})
