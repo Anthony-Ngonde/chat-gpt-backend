b8663badc3baf1a1bc80912aa006e66d997086e2
Anthony Ngonde <anto.ngonde@gmail.com>
2024-11-06 10:39:23 +0300

    Fixed a bug in backend

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import express from "express"
+import express from "express";
 import cors from "cors"
 import ImageKit from "imagekit";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin:process.env.CLIENT_URL 
+    origin:process.env.CLIENT_URL
 }))
+
 
 const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
     publicKey: process.env.IMAGE_KIT_PUBLIC_KEY ,
     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY 
-  });
+  }); 
 
 app.get("/api/upload",(req,res)=>{
     const result = imagekit.getAuthenticationParameters();
@@ -21,6 +28,7 @@ app.get("/api/upload",(req,res)=>{
 })
 
 
-app.listen(port,()=>{
+app.listen(port,() => {
     console.log("Server running on 3000")
 })
+
